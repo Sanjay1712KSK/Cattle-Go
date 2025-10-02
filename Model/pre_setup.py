1cba1b41652576fe505e2d4536b233e98810aa13
Sanjay Kumar S <142003873+Sanjay1712KSK@users.noreply.github.com>
2025-10-03 01:37:05 +0530

    Delete Model directory

--- Model/pre_setup.py
+++
@@ -1,8 +0,0 @@
-import os
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Dropout, Flatten, Dense
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
